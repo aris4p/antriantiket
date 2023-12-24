8ffc82f52c17a3c25a87794d2d08b1d459481527
Enoir <Enoir@DESKTOP-O4IR1N2>
2023-12-24 14:07:06 +0700

    Middleware Group

--- routes/web.php
+++ routes/web.php
@@ -26,18 +26,23 @@ Route::get('/antrian', function(){
     return view('index');
 });
 
+
+
 Route::get('/tiket', [TiketController::class, 'index']);
 Route::post('/tiket', [AntrianController::class, 'create'])->name('create-tiket');
 
 Route::get('/login', [AuthController::class, 'index']);
 Route::post('/login', [AuthController::class, 'auth'])->name('login');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::post('/loket', [DashboardController::class, 'loket'])->name('loket');
-
-Route::get('/antri', [AntrianController::class, 'index'])->name('daftar-antrian');
-Route::post('/antri/getantrian', [AntrianController::class, 'getAntrian'])->name('get-antrian');
-Route::get('/antri/currentantrian', [AntrianController::class, 'currentantrian'])->name('current-antrian');
-Route::post('/antri/antrianselesai', [AntrianController::class, 'antrianselesai'])->name('antrian-selesai');
-
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::middleware(['auth'])->group(function () {
+    // Your authenticated routes go here
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::post('/loket', [DashboardController::class, 'loket'])->name('loket');
+    
+    Route::get('/antri', [AntrianController::class, 'index'])->name('daftar-antrian');
+    Route::post('/antri/getantrian', [AntrianController::class, 'getAntrian'])->name('get-antrian');
+    Route::get('/antri/currentantrian', [AntrianController::class, 'currentantrian'])->name('current-antrian');
+    Route::post('/antri/antrianselesai', [AntrianController::class, 'antrianselesai'])->name('antrian-selesai');
+    
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+});
